e83b8b2e95e229bd2ce53fca2f12d734a74b3d52
Tesla0802 <99128017+Tesla0802@users.noreply.github.com>
2022-03-01 19:57:24 +0400

    slider end

--- script.js
+++ script.js
@@ -20,13 +20,21 @@ var swiper = new Swiper(".mySwiper", {
     el: ".swiper-pagination",
     clickable: true,
   },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
   breakpoints: {
     640: {
       slidesPerView: 2,
       spaceBetween: 20,
     },
     768: {
-      slidesPerView: 4,
+      slidesPerView: 2,
       spaceBetween: 40,
     },
     1024: {
