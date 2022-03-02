0828af3101506f2e1efdd8d3a7fdf4b0b0e4c4bd
Tesla0802 <99128017+Tesla0802@users.noreply.github.com>
2022-03-02 14:46:30 +0400

    responsive

--- script.js
+++ script.js
@@ -13,6 +13,19 @@
 //     return { name: user.name, email: user.email, element: card };
 //   });
 
+let scrollPrecentage = () => {
+  let scrollProgress = document.getElementById("progress");
+  let progressValue = document.getElementById("progress-value");
+  let pos = document.documentElement.scrollTop;
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+  scrollProgress.style.background = `conic-gradient(#009c7c ${scrollValue}%, #2b2f38 ${scrollValue}%)`;
+};
+window.onscroll = scrollPrecentage;
+window.onload = scrollPrecentage;
+
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   spaceBetween: 10,
@@ -42,4 +55,11 @@ var swiper = new Swiper(".mySwiper", {
       spaceBetween: 50,
     },
   },
+});
+$(window).scroll(function () {
+  if ($(document).scrollTop() > 20) {
+    $(".navigation").addClass("fix-icon");
+  } else {
+    $(".navigation").removeClass("fix-icon");
+  }
 });
