b46e7a5d47486a2e93022acb7800b5ffa78663c0
Tesla0802 <99128017+Tesla0802@users.noreply.github.com>
2022-03-06 19:18:06 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// const searchinput = document.querySelector(["data-search"]);
-// searchinput.addEventListener("input", (e) => {
-//   const value = e.target.value;
-//   user.forEach((user) => {
-//     const isVisible = user.name.includes(value) || user.emale.includes(value);
-//     user.element.classlist.taggle("hide", !isVisible);
-//   });
-// });
-
-// fetch("https://jsonplaceholder.typicode.com/users")
-//   .then((res) => res.json())
-//   .then((data) => {
-//     return { name: user.name, email: user.email, element: card };
-//   });
-
 let scrollPrecentage = () => {
   let scrollProgress = document.getElementById("progress");
   let progressValue = document.getElementById("progress-value");
